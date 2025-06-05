e8d50e4293da5d31ff002facbb0996b6d7dfac7c
PasyaMaulana <pasyamaulana17@gmail.com>
2025-06-05 11:25:17 +0700

    case choice 3

--- stack.cpp
+++ stack.cpp
@@ -102,6 +102,9 @@ int main()
         case 2:
             stack.pop(); // Pop the topmost element from the stack
             break;
+        case 3:
+            stack.peek();
+            break;
         }
 
         cout << endl;
