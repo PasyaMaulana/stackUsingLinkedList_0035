091ce771b58a4e7994d76d453b6834f5cb7a80e4
PasyaMaulana <pasyamaulana17@gmail.com>
2025-06-05 11:16:14 +0700

    Pop operation

--- stack.cpp
+++ stack.cpp
@@ -43,4 +43,9 @@ public:
     {
         return top == NULL; // Return true if the top pointer is NULL, indicating an empty stack
     }
+
+    // Pop operation: Remove the topmost element from the stack
+    void pop()
+    {
+    }
 };
